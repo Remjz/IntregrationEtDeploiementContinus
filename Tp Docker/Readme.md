25ad571bc43948f56023d13e382858134bf1da34
Remjz <remi.jauzion81@gmail.com>
2025-09-05 07:03:17 +0000

    Exo6

--- Tp Docker/Readme.md
+++ Tp Docker/Readme.md
@@ -1,3 +1,7 @@
 docker build -t flask-hello-world .
 
 docker run -p 8080:5000 flask-hello-world
+
+exo 6 : 
+
+docker compose -f "Tp Docker/docker-compose.yml" up -d --build
--- Tp Docker/app.py
+++ Tp Docker/app.py
@@ -1,10 +1,29 @@
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello_world():
+#     return 'Hello, World!'
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=5000)
+
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+client = MongoClient("mongodb://localhost:27017/")
+db = client["ma_base"]  # nom de la base
+collection = db["messages"]  # nom de la collection
+
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    collection.insert_one({"message": "Hello, World!"})
+    last_message = collection.find().sort("_id", -1).limit(1)
+    msg = next(last_message, {"message": "Hello, World!"})["message"]
+    return msg
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
